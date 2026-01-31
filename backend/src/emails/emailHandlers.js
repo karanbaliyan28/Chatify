550530555932e763794ec60cdf3a46474d79a30d
karan baliyan <karanbaliyan62@gmail.com>
2026-01-31 22:13:10 +0530

    final

--- backend/src/emails/emailHandlers.js
+++ backend/src/emails/emailHandlers.js
@@ -1,18 +1,34 @@
-import { resendClient, sender } from "../lib/resend.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+import nodemailer from "nodemailer";
 import { createWelcomeEmailTemplate } from "../emails/emailTemplates.js";
 
-export const sendWelcomeEmail = async (email, name, clientURL) => {
-  const { data, error } = await resendClient.emails.send({
-    from: `${sender.name} <${sender.email}>`,
-    to: email,
-    subject: "Welcome to Chatify!",
-    html: createWelcomeEmailTemplate(name, clientURL),
-  });
+const transporter = nodemailer.createTransport({ 
+  service:"gmail",
+  auth: {
+    user: process.env.ADMIN_EMAIL,   
+    pass: process.env.ADMIN_EMAIL_PASSWORD,   
+  },
+});
 
-  if (error) {
+const sender = {
+  name: "Chatify",
+  email: process.env.ADMIN_EMAIL,
+};
+
+export const sendWelcomeEmail = async (email, name, clientURL) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `${sender.name} <${sender.email}>`,
+      to: email,
+      subject: "Welcome to Chatify!",
+      html: createWelcomeEmailTemplate(name, clientURL),
+    });
+
+    console.log("Welcome Email sent successfully:", info.messageId);
+  } catch (error) {
     console.error("Error sending welcome email:", error);
     throw new Error("Failed to send welcome email");
   }
-
-  console.log("Welcome Email sent successfully", data);
 };
