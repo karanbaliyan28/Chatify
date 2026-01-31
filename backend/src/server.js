98873fe40890bdeec10e3f498b8af8133bfc6789
karan baliyan <karanbaliyan62@gmail.com>
2026-01-31 23:24:19 +0530

    as

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,46 +1,38 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
+const __dirname = path.resolve();
 
-// Connect DB once (important for serverless)
-let isConnected = false;
-app.use(async (req, res, next) => {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
-  next();
-});
+const PORT = ENV.PORT || 3000;
 
-app.use(express.json({ limit: "5mb" }));
-
-app.use(cors({
-  origin: ENV.CLIENT_URL,
-  credentials: true,
-}));
-
-app.use(
-  cors({
-    origin: ENV.CLIENT_URL,
-    credentials: true,
-  }),
-);
-
+app.use(express.json({ limit: "5mb" })); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
+app.get("/" ,(req,res)=>{
+  res.redirect("/api/auth");
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// test route
-app.get("/api/health", (req, res) => {
-  res.json({ status: "ok" });
-});
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-export default app;
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
+});
