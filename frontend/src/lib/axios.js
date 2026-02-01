f0047c653f9028bd04a5649022857375f7cd9c5d
KARAN BALIYAN <karanbaliyan162@gmail.com>
2026-02-01 15:23:39 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
-    : "https://pingbackend-s4co.onrender.com";
+    : "https://pingbackend-s4co.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
