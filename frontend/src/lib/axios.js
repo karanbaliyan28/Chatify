e87d4e6771830bc554af99b1ee4f87f3e3047614
karan baliyan <karanbaliyan62@gmail.com>
2026-02-01 14:42:54 +0530

    l

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
-    : "https://chatify12.onrender.com/api"; 
+    : "https://chatify12.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
